4e62ac2d791bda45956bc7766c202df8435d553d
Peter Shoukry <peter.shoukry@udin.io>
2026-09-18 20:25:42 +0300

    chore: release 0.5.2
    Bumps @version to 0.5.2 and dates the CHANGELOG's Unreleased section,
    which already carried the #66 tuple fix on main since PR #88.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@
 defmodule AshIntrospection.MixProject do
   use Mix.Project
 
-  @version "0.5.1"
+  @version "0.5.2"
 
   @description """
   Shared core library for Ash interoperability with multiple languages.
